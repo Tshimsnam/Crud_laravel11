f60124e6f0c8b88311c457422197ce9e825939ac
Tshimsnam <manassetshims@gmail.com>
2024-07-18 16:09:16 +0100

    configuration du  AppserviceProvider.php pour reglé le probleme de la clé est trop longue definitions des differentes methodes pour le crud

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -3,16 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Etudiant;
 
 class EtudiantController extends Controller
 {
     public function list_etudiant()
     {
-        return view('etudiants.liste');
+        $etudiants = Etudiant::all();
+        return view('etudiants.liste', compact('etudiants'));
     }
 
     public function ajouter_etudiant()
     {
         return view('etudiants.ajouter');
+    }
+
+    public function ajouter_etudiant_traitement(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+            'classe' => 'required'
+        ]);
+        $etudiant = new Etudiant();
+        $etudiant->nom = $request->nom;
+        $etudiant->prenom = $request->prenom;
+        $etudiant->classe = $request->classe;
+        $etudiant->save();
+
+        return redirect('/ajouter')->with('status', 'L\'étudiant à bien été ajouter avec succès');
+    }
+
+    public function update_etudiant($id){
+        $etudiants = Etudiant::find($id);
+        return view('etudiants.update', compact('etudiants'));
+    }
+
+    public function update_etudiant_traitement(Request $request){
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+            'classe' => 'required'
+        ]);
+        $etudiant = Etudiant::find($request->id);
+        $etudiant->nom = $request->nom;
+        $etudiant->prenom = $request->prenom;
+        $etudiant->classe = $request->classe;
+        $etudiant->update();
+
+        return redirect('/etudiant')->with('status', 'L\'étudiant a bien été modifié avec succès');
     }
 }
